0facce9301269780a4c0758bbde23d2a95bfd154
s01ver <solverglace@gmail.com>
2023-12-25 17:34:48 +0900

    Initial Commit

---
+++ iosstudy6-2/UIView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.swift
+//  iosstudy6-2
+//
+//  
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
---
+++ iosstudy6-2/UserInfo.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfo.swift
+//  iosstudy6-2
+//
+//  
+//
+
+import Foundation
+
+struct UserInfo {
+    let email: String
+    let name: String
+    let nickname: String
+    let password: String
+}
+
